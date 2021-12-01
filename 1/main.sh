bfc49c9e388fd1fd9143097c10e223e5039eea52
Evan do Carmo <carmo.evan@gmail.com>
2021-12-01 09:15:23 -0500

    fix

--- 1/main.sh
+++ 1/main.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 input="./input.txt"
-prev=""
+prev=0
 count=0
 while read -r line
 do 
     curr=$line
-    if [ $prev != "" -a $curr -gt $prev ]; then
+    if [ $prev != 0 -a $curr -gt $prev ]; then
         count=$((count+1))
     fi
     prev=$line
